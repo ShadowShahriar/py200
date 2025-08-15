a625587113183352ff11805db4db9c8c78fbda06
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-08-15 19:40:11 +0600

    ✅ The main function

---
+++ 04-main-fn/main.py
@@ -0,0 +1,15 @@
+def fn1() -> None:
+    print("Cool stuff")
+
+
+def fn2() -> None:
+    print("Some more cool stuff")
+
+
+def main() -> None:
+    fn1()
+    fn2()
+
+
+if __name__ == "__main__":
+    main()
