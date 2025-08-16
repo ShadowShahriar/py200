15bede55ce93157509bd90b55cc567f8005ae7e6
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-08-16 20:05:21 +0600

    ✅ File: Open File Dialog

---
+++ 08-file/05-choose-file.py
@@ -0,0 +1,10 @@
+import filedialpy
+
+f = filedialpy.openFile(
+    initial_dir="C:\\Windows\\System32",
+    initial_file="hello.txt",
+    title="Choose a file",
+    filter=["*.txt *.json", "*.txt", "*.json"],
+)
+
+print(f)
