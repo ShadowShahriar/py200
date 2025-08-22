7e0b769d3509dbff8bf212dfd231705c3cdbdd78
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-08-23 01:51:47 +0600

    ✅ Basics: Activity 29

---
+++ 05-basics/29-built-in-functions.py
@@ -0,0 +1,11 @@
+x = [3, 12, 64, 32]
+print(f"{len(x) = }")
+print(f"{max(x) = }")
+print(f"{min(x) = }")
+print(f"{sum(x) = }")
+
+print("")
+print(f"{sorted(x) = }")
+print(f"{list(range(1, 10, 1)) = }")
+print(f"{list(zip(range(1, 5), range(5, 10))) = }")
+print(f"{list(reversed(range(1, 10, 1))) = }")
