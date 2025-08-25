19f6334215b14f27b538900e8fa5b332041d4cc4
ShadowShahriar <shadowshahriar.dev@gmail.com>
2025-08-25 23:55:39 +0600

    ✅ NumPy: The basics

---
+++ 13-numpy/03-math-fn.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = np.array([1, 2, 3, 4])
+
+print(np.sqrt(x))
+print(np.square(x))
+print(np.exp(x))
+print(np.log(x))
+print(np.sin(x))
+print(np.cos(x))
---
+++ 13-numpy/04-tuple-operations.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+print(np.zeros((3, 3)))
+print("")
+
+print(np.ones((2, 2)))
+print("")
+
+print(np.full((2, 3), 7))
+print("")
+
+print(np.eye(4))
+print("")
+
+print(np.arange(0, 10, 2))
+print("")
+
+print(np.linspace(0, 1, 5))
